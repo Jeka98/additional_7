0363706974295c6e9e2dd5f5d547443f19a3ecaf
Jeka98 <Zhukjeka1998@yandex.by>
2018-10-04 19:32:29 +0300

    solve sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+  for (let x = 0; x < 9; x++) {
+    for(let y = 0; y < 9; y++) {
+      if(matrix[x][y] === 0) {
+        let values = getValue(x, y, matrix);
+        for (let getValue of values) {
+          matrix[x][y] = getValue;
+        }
+      }
+    }
+  }
+
+  function getValue(x, y, matrix){
+    let tempArr = [];
+    let row = Math.floor(x / 3) * 3;
+    let col = Math.floor(y / 3) * 3;
+    for (let i = 0; i < 9; i++){
+      tempArr.push([matrix[row][i], matrix[i][col], matrix[row + i % 3][col + i % 3]]);
+    }
+    return tempArr;
+  }
+
+  return matrix;
+};
